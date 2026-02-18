056f5a1d9598768afc12f4a5a5d05b462ddb484d
tradermathias <trader.mathias@proton.me>
2026-02-18 10:26:41 -0500

    Delete wsgi.py.py

--- wsgi.py.py
+++
@@ -1,9 +0,0 @@
-"""
-WSGI wrapper for Render deployment.
-This allows Render to find the app object easily.
-"""
-
-from agentic_trust_dashboard import app
-
-if __name__ == "__main__":
-    app.run(debug=False)
