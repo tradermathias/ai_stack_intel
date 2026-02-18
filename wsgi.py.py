ea58ddd8f4cbbbabefedce53e50fda40dfc18c31
tradermathias <trader.mathias@proton.me>
2026-02-18 10:26:16 -0500

    Add files via upload

---
+++ wsgi.py.py
@@ -0,0 +1,9 @@
+"""
+WSGI wrapper for Render deployment.
+This allows Render to find the app object easily.
+"""
+
+from agentic_trust_dashboard import app
+
+if __name__ == "__main__":
+    app.run(debug=False)
